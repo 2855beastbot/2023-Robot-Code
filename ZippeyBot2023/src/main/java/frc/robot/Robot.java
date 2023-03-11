72d0088d6e648413694ff9768f6b864a7ee68afe
keeperofhoney <120990347+keeperofhoney@users.noreply.github.com>
2023-03-11 12:19:49 -0600

    Added secondary camera

--- ZippeyBot2023/src/main/java/frc/robot/Robot.java
+++ ZippeyBot2023/src/main/java/frc/robot/Robot.java
@@ -44,7 +44,8 @@ public class Robot extends TimedRobot {
         // autonomous chooser on the dashboard.
         m_robotContainer = RobotContainer.getInstance();
         HAL.report(tResourceType.kResourceType_Framework, tInstances.kFramework_RobotBuilder);
-        CameraServer.startAutomaticCapture();
+        CameraServer.startAutomaticCapture(0);
+        CameraServer.startAutomaticCapture(1);
         
     }
 
