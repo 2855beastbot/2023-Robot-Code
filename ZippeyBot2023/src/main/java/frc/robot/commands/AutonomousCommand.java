38e11a25dd6227a4854ba2a6fec289af3c36a03e
2855beastbot <tristan.flynn.engelmann@gmail.com>
2023-03-20 16:15:58 -0500

    Auto Drive test
    Not yet tested

--- ZippeyBot2023/src/main/java/frc/robot/commands/AutonomousCommand.java
+++ ZippeyBot2023/src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -51,6 +51,7 @@ public class AutonomousCommand extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        
     }
 
     // Called once the command ends or is interrupted.
